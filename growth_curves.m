dc72a87080c2147a19b0a9af5e046b6512e78b4a
nkylilis <36237120+nkylilis@users.noreply.github.com>
2018-02-20 18:16:34 +0000

    Add files via upload

---
+++ growth_curves.m
@@ -0,0 +1,42 @@
+function OD700_data_row = growth_curves(x1, x2, OD700_data, time)
+
+t = length(time);
+OD700_data_row = OD700_data(1:end,x1:x2);
+
+    for i= 1:12
+        
+        for n=2:t
+            
+            if OD700_data_row(n,i)<(1.25*OD700_data_row(1,i))
+                OD700_data_row(n,i)=(1.25*OD700_data_row(1,i));
+            end
+            
+        end
+        
+        OD700_data_row(1,i) = OD700_data_row(2,i);
+        
+        
+    end
+    
+OD700_data_row = OD700_data_row - OD700_data_row(1,1);
+
+OD700_data_row(OD700_data_row<0.01) = 0.01;
+
+a = [time OD700_data_row];
+
+fig = figure;
+for i = 1:12
+    subplot(3,4,i)
+    plot(time, OD700_data_row(:,i),'.')
+    xlabel('time (minutes)')
+    ylabel('OD700')
+    ylim([0 0.5]);
+    xlim([0 1500]);
+    title('Sample: '  + string(x1+i) )
+end
+saveas(fig, 'Growth curves- Samples: '  + string(x1) + '-' + string(x2))
+close(fig);
+
+xlswrite('growth_curves - Samples: ' + string(x1) + '-' + string(x2), a);
+
+end
--- main.m
+++ main.m
@@ -1,9 +1,42 @@
+% matrices that include experimental data
+time = xlsread('OD700_data','Sheet1','A1:A70');
+OD700_data = xlsread('OD700_data','Sheet1','B1:CS70');
+GFP_data = xlsread('GFP_data','Sheet1','B1:CS70');
+timestep = 0.33; % time difference(in hrs) difference between successive measurements
 
-x1 = 1;         %well
-x2 = 12;        %well
 
-[max_growth_rates, time_index, max_dt, f1, f2, f3] = growth_rate4(x1, x2);
+% matrix initialisation that will contain the experimental results
+growth_rate_matrix = zeros(8,12); 
+GFP_capacity_matrix = zeros(8,12);
 
-[GFP_rate_max_v, fGFP1,fGFP2,fGFP3] = Normalised_GFP_fluorescence(x1,x2, time_index);
 
-output = [max_growth_rates time_index GFP_rate_max_v]
+% loop iterates through rows A-H of the 96-well microplate to analyse data
+i = 1;
+x1 = 1;         %well A1
+x2 = 12;        %well A12
+for i = 1:8
+    
+    % analysis of cell growth
+    OD700_data_row = growth_curves(x1, x2, OD700_data, time);
+    [max_gr, index] = growth_rate(time, OD700_data_row, x1, x2, timestep);
+    matrixGR(i,:) = max_gr;
+       
+    
+    % analysis of GFP fluorescence
+    [GFP_per_cell_max_gr, GFP_per_cell_values] = GFP_per_cell(GFP_data, x1, x2, index, OD700_data_row, time);
+    matrixGFP(i,:) = GFP_per_cell_max_gr;
+    rate_matrix = GFP_production_rate_per_cell(time, GFP_per_cell_values, timestep, x1, x2);
+       
+    
+    % analysis of red fluorescence
+    % to be added
+    
+    
+    x1 = x1 + 12;
+    x2 = x2 + 12;
+    
+    i = i+1;
+    
+end
+xlswrite('max_growth_rate_analysis', matrixGR);
+xlswrite('GFP_per_cell_@maxGR', matrixGFP);
