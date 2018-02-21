294752d747c19dd047bbc126a2381828a00ed644
Nicolas Kylilis <36237120+nkylilis@users.noreply.github.com>
2018-02-21 12:11:32 +0000

    Add files via upload

--- growth_rate.m
+++ growth_rate.m
@@ -1,17 +1,17 @@
-function [max_growth_rates, index] = growth_rate(time, OD700, x1, x2, timestep)
+function [max_growth_rates, index] = growth_rate(time, OD700_data_row, x1, x2, timestep)
 
-ln_OD700 = log(OD700);
+ln_OD700_row = log(OD700_data_row);
 
-time_gr = time(3:end-2);
+time_gr = time(3:end-1);
 k = zeros(length(time_gr),12);
 
 for g = 1:12                          
     
     v = zeros(1,length(time_gr));
     
-    for i = 2:length(time_gr)
-        A = [ln_OD700(i+2,g)  ln_OD700(i+3,g) ln_OD700(i+4,g)];
-        B = [ln_OD700(i,g) ln_OD700(i+1,g) ln_OD700(i+2,g)];
+    for i = 3:(length(time)-3)
+        A = [ln_OD700_row(i,g)  ln_OD700_row(i-1,g) ln_OD700_row(i+1,g)];
+        B = [ln_OD700_row(i-1,g) ln_OD700_row(i-2,g) ln_OD700_row(i,g)];
         y = mean(A) - mean(B);
         growth_r = y/(2*timestep);
         v(1,i)=growth_r;
@@ -19,8 +19,8 @@ for g = 1:12
     k(:,g) = v;
     
 end
-c=zeros(2,12);
-k = [c; k; c];
+c=zeros(1,12);
+k = [c; c; k; c];
 fig = figure;
 for i = 1:12
     subplot(3,4,i)
@@ -31,7 +31,7 @@ for i = 1:12
     xlim([0 1500]);
     title('Sample: '  + string(x1+i) )
 end
-saveas(fig, 'Growth rate- Samples: '  + string(x1) + '-' + string(x2))
+print('Growth rate- Samples: '  + string(x1) + '-' + string(x2),'-dpng')
 close(fig);
 
 xlswrite('growth_rate_analysis - Samples: ' + string(x1) + '-' + string(x2), k);
